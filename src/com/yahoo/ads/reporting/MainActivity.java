791993844629f7dbe9e8395e9520beaba96941ce
Pulkit <pulkitg@yahoo-inc.com>
2014-07-17 02:09:28 -0700

    Added app with basic screens

---
+++ src/com/yahoo/ads/reporting/MainActivity.java
@@ -0,0 +1,81 @@
+package com.yahoo.ads.reporting;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends ActionBarActivity implements OnClickListener {
+
+	private Button trendsButton;
+	private Button vendorsButton;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		trendsButton = ((Button) findViewById(R.id.button1));
+		vendorsButton = ((Button) findViewById(R.id.button2));
+
+		trendsButton.setOnClickListener(this);
+		vendorsButton.setOnClickListener(this);
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	public void onClick(View v) {
+		Intent intent;
+		switch (v.getId()) {
+		case R.id.button1:
+			intent = new Intent(this, TrendActivity.class);
+			startActivity(intent);
+			break;
+		case R.id.button2:
+			intent = new Intent(this, VendorListActivity.class);
+
+			ArrayList<String> vendors = new ArrayList<String>();
+			vendors.add("vendor1");
+			vendors.add("vendor2");
+			vendors.add("vendor3");
+			vendors.add("vendor4");
+			vendors.add("vendor5");
+			intent.putStringArrayListExtra("vendors", vendors);
+
+			startActivity(intent);
+			break;
+
+		default:
+			break;
+		}
+
+	}
+
+}
---
+++ src/com/yahoo/ads/reporting/TrendActivity.java
@@ -0,0 +1,114 @@
+package com.yahoo.ads.reporting;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class TrendActivity extends ActionBarActivity {
+
+	EditText etResponse;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_trend);
+		// get reference to the views
+		etResponse = (EditText) findViewById(R.id.editText1);
+
+		// call AsynTask to perform network operation on separate thread
+		new HttpAsyncTask()
+				.execute("http://abysmaldismal.corp.ir2.yahoo.com:4080/latency/v1/fallOutReport");
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.trend, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	public static String GET(String url) {
+		InputStream inputStream = null;
+		String result = "";
+		try {
+
+			// create HttpClient
+			HttpClient httpclient = new DefaultHttpClient();
+
+			// make GET request to the given URL
+			HttpResponse httpResponse = httpclient.execute(new HttpGet(url));
+
+			// receive response as inputStream
+			inputStream = httpResponse.getEntity().getContent();
+
+			// convert inputstream to string
+			if (inputStream != null)
+				result = convertInputStreamToString(inputStream);
+			else
+				result = "Did not work!";
+
+		} catch (Exception e) {
+			Log.d("InputStream", e.getLocalizedMessage());
+		}
+
+		return result;
+	}
+
+	private static String convertInputStreamToString(InputStream inputStream)
+			throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(
+				new InputStreamReader(inputStream));
+		String line = "";
+		String result = "";
+		while ((line = bufferedReader.readLine()) != null)
+			result += line;
+
+		inputStream.close();
+		return result;
+
+	}
+
+	private class HttpAsyncTask extends AsyncTask<String, Void, String> {
+		@Override
+		protected String doInBackground(String... urls) {
+
+			return GET(urls[0]);
+		}
+
+		// onPostExecute displays the results of the AsyncTask.
+		@Override
+		protected void onPostExecute(String result) {
+			Toast.makeText(getBaseContext(), "Received!", Toast.LENGTH_LONG)
+					.show();
+			etResponse.setText(result);
+		}
+	}
+}
